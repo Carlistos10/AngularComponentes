5ff41b4fc9494e6359e2b00bfac6c016d70cff20
Carlistos10 <Maranhasgc@gmail.com>
2024-05-28 19:20:23 +0200

    reloj y calendario

--- angular-componentes/src/app/widget2/widget2.component.ts
+++ angular-componentes/src/app/widget2/widget2.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+
+import moment from 'moment';
 
 
 @Component({
@@ -14,10 +16,22 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   templateUrl: './widget2.component.html',
   styleUrl: './widget2.component.css'
 })
-export class Widget2Component {
+export class Widget2Component implements OnInit {
+
   durationInSeconds = 2;
 
-  constructor(private _widgetBar: MatSnackBar) { }
+  firstTime: string;
+  hour!: string;
+
+  constructor(private _widgetBar: MatSnackBar) {
+    this.firstTime = moment().format('HH: mm: ss');
+  }
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.hour = moment().format('HH: mm: ss');
+    }, 1000);
+  }
 
   openWidgetGuardado() {
     this._widgetBar.openFromComponent(WidgetAnadido, {
